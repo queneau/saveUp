a8724257e89b41c102f8984309a5fa6aa9f58ad5
queneau <risakno3@gmail.com>
2020-01-13 17:55:07 +0900

    Initial Commit

---
+++ saveUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  saveUp
+//
+//  Created by Risako Kuno on 2020/01/13.
+//  Copyright © 2020 kno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
